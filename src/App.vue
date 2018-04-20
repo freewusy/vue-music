080940c73c26d31b02ef05f2516cde0c966bf9c7
wusy5 <wusy5@centaline.com.cn>
2018-04-20 16:19:52 +0800

    commit first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+export default {
+  name: 'app',
+  components: {
+    MHeader
+  }
+}
+</script>

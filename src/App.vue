23404b8e77b752c9e8bf37d4cacd65ebf0e2c431
freewusy <wushuyang92@126.com>
2018-04-20 20:02:08 +0800

    commit two

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+
+export default {
+  name: 'app',
+  components: {
+    MHeader
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import MHeader from '@/components/m-header'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'MHeader',
+//       component: HelloWorld
+//     }
+//   ]
+// })
